a3afb19549673b59d30862bb1f9737675caad00c
omarcovarrubias <omargeovanigc@gmail.com>
2021-07-09 01:25:00 -0500

    Se agrego otro boton

---
+++ index.js
@@ -0,0 +1,10 @@
+elemento = document.getElementById(id);
+function changeColor(newColor) {
+    var elem = document.getElementById('para');
+    elem.style.color = newColor; 
+}
+
+ function ventana() {     
+     alert('Hola mundo');
+
+ }
