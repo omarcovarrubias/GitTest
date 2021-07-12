54c43f1f30f044852dd1faeb67bc96a2363a2688
omarcovarrubias <omargeovanigc@gmail.com>
2021-07-12 14:07:25 -0500

    agregando una funcion request

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function changeColor(newColor) {
      alert('Hola mundo');
 
  }
+
+ function getPosts() {
+    return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+        req.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+        req.onload = function() {
+          if (req.status == 200) {
+            resolve(JSON.parse(req.response));
+          }
+          else {
+            reject();
+          }
+        };
+
+        req.send();
+    })
+}
+
+getPosts().then(r =>{
+    console.log(r);
+  }).catch(() => {
+    console.log('Algo salió mal');
+  });
